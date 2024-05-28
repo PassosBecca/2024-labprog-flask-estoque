454e038379b47fb5159b8d604ff8b7622be5f8c7
Rebeca <rebeca.crist1@hotmail.com>
2024-05-28 19:10:30 -0300

    Aula quarta 23 Produto e Categorias

--- src/models/categoria.py
+++ src/models/categoria.py
@@ -1,20 +1,21 @@
 import uuid
 from sqlalchemy import String, Uuid
 from sqlalchemy.orm import Mapped, mapped_column, relationship
-from src.models.base_mixin import (BasicRepositoryMixin,
-                                   TimeStampMixin)
+from src.models.base_mixin import (BasicRepositoryMixin, TimeStampMixin)
+
 from src.modules import db
 
 
 class Categoria(db.Model, BasicRepositoryMixin, TimeStampMixin):
     __tablename__ = 'categorias'
+
     id: Mapped[Uuid] = mapped_column(Uuid(as_uuid=True),
                                      primary_key=True,
                                      default=uuid.uuid4)
     nome: Mapped[str] = mapped_column(String(128),
                                       nullable=False)
 
-    lista_de_prosutos = relationship('Produto',
+lista_produtos = relationship('Produto',
                                      back_populates='categoria',
                                      cascade='all, delete-orphan',
                                      lazy='select')
